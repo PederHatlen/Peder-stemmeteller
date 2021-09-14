792b1f00b619d331dfdeb201e3188af3f60d696e
PederHatlen <pehaa002@osloskolen.no>
2021-09-14 11:30:43 +0200

    Added Insert Data

---
+++ SQL/InsertData.sql
@@ -0,0 +1,9 @@
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Arbeiderpartiet', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Høyre', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Fremskrittspartiet', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Senterpartiet', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Sosialistisk Venstreparti', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Venstre', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Kristelig Folkeparti', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Miljøpartiet De Grønne', 0);
+INSERT INTO `stemmer`(`parti`, `stemmer`) VALUES ('Rødt', 0);
