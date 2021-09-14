40665d83d554742257627e03364e46a6fedf61f7
PederHatlen <pehaa002@osloskolen.no>
2021-09-14 11:31:12 +0200

    Fixed teble code

--- SQL/Table.sql
+++ SQL/Table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE stemmer (
-    parti_id int PRIMARY KEY,
+    parti_id int AUTO_INCREMENT,
     parti VARCHAR(255) NOT NULL,
-    stemmer int,
-    table_constraints
+    stemmer int
+    PRIMARY KEY (parti_id)
 );
